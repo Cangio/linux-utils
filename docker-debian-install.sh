d65b04ce252f5727447f4fd7dfb949d294977e7a
Simone <simone@simonecangini.it>
2022-10-25 14:55:55 +0200

    Added docker-compose installation

--- docker-debian-install.sh
+++ docker-debian-install.sh
@@ -14,5 +14,9 @@ sudo apt-get install -yy docker-ce docker-ce-cli containerd.io docker-compose-pl
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 mkdir ~/dockers
 touch ~/dockers/.env
